8fc5e07c0edd87114fc3aba34063dcb8e3328276
Satheesh Madupathi <smadupathi@osius.com>
2018-12-29 20:54:19 +0530

    Initial Commit

---
+++ PriestApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using PriestApp.API.Models;
+
+namespace PriestApp.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+
+        public DbSet<Value> Values {get;set;}
+        
+    }
+}
