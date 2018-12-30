919d242d18d5a5b88420bbc66607fdd84d6f3213
Satheesh Madupathi <smadupathi@osius.com>
2018-12-30 21:29:06 +0530

    Security Commit

--- PriestApp.API/Dtos/UserForRegisterDto.cs
+++ PriestApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PriestApp.API.Dtos
 {
     public class UserForRegisterDto
-    {
+    {  
+        [Required]
         public string Username { get; set; }
+        
+        [Required]
+        [StringLength(8,MinimumLength=4,ErrorMessage="Password Length minimum 4 and Maximum 8 Chars")]
         public string Password { get; set; }
 
         //Dto-- means Data Transfer Object
